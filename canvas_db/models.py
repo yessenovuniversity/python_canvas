c1e12c62492d63165673e10dda3e23a7a4966274
Nauryzbek Aitbayev <nauryzbek.aitbayev@yu.edu.kz>
2020-10-24 12:48:23 +0500

    Added CourseSection

--- canvas_db/models.py
+++ canvas_db/models.py
@@ -23,3 +23,24 @@ class Course(Base):
     
     def __str__(self):
         return self.name
+
+
+class CourseSection(Base):
+    """
+    Модель "Секция курса"
+    """
+
+    __tablename__ = 'course_sections'
+
+    id = Column(Integer, primary_key=True)
+    sis_source_id = Column(String(255))
+    course_id = Column(ForeignKey('courses.id'))
+    course = relationship('Course')
+    name = Column(String(255))
+    workflow_state = Column(String(255))
+
+    def __repr__(self):
+        return '<CourseSection {} (id={}, sis_source_id={})>'.format(self, self.id, self.sis_source_id)
+    
+    def __str__(self):
+        return self.name

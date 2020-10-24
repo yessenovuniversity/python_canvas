eb7dda7ebc7d1b43cb06b19f138f26654275ff9b
Nauryzbek Aitbayev <nauryzbek.aitbayev@yu.edu.kz>
2020-10-24 13:54:08 +0500

    Added ContextModule and ContentTag

--- canvas_db/models.py
+++ canvas_db/models.py
@@ -44,3 +44,42 @@ class CourseSection(Base):
     
     def __str__(self):
         return self.name
+
+
+class ContextModule(Base):
+    """
+    Модель "Модули курса"
+    """
+
+    __tablename__ = 'context_modules'
+
+    id = Column(Integer, primary_key=True)
+    context_id = Column(Integer)
+    context_type = Column(String(255))
+    name = Column(String)
+    position = Column(Integer)
+    workflow_state = Column(String(255))
+
+    def __repr__(self):
+        return '<ContextModule {} (id={})>'.format(self, self.id)
+    
+    def __str__(self):
+        return self.name
+
+
+class ContentTag(Base):
+    __tablename__ = 'content_tags'
+
+    id = Column(Integer, primary_Key=True)
+    content_id = Column(Integer)
+    content_type = Column(String(255))
+    title = Column(String)
+    context_module_id = Column(ForeignKey('context_modules.id'))
+    context_module = relationship('ContextModule')
+    workflow_state = Column(String(255))
+
+    def __repr__(self):
+        return '<ContentTag {} (id={})>'.format(self, self.id)
+    
+    def __str__(self):
+        return self.title
